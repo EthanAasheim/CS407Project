ddb8d925255e39e8cd2a19eb0ef07d4189231f6b
Ethan Aasheim <ethanaasheim@icloud.com>
2022-11-04 22:58:55 -0700

    Added first file - nothing in it for now though

---
+++ start.py
@@ -0,0 +1,13 @@
+"""
+
+Hello world
+
+I've found an open-source Python library for controlling my specific brand of light bulbs here:
+https://github.com/mclarkk/lifxlan
+
+I've cloned the repo and tested it (I'm out of town so I don't have my lightbulbs to test, but the code runs).
+
+Sorry for the lack of progress - midterms and other classes have taken up more time than I expected.
+I will work on this Wednesday the 9th when I am back in Eugene and don't have pressing projects in other courses.
+
+"""
