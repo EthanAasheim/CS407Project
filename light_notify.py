718cc685afbe7e7af0612ceccfb8871a7f6284d2
Ethan Aasheim <ethanaasheim@icloud.com>
2022-12-02 00:40:20 -0800

    Can correctly get weather data, added temperature -> color function

--- light_notify.py
+++ light_notify.py
@@ -6,41 +6,60 @@ Module that sets light color, fade duration, and brightness
 
 
 import sys
-import time
-import colorsys
+import os
+import argparse
 
-from pynput.keyboard import Key, Controller
+import time
+import select
+
+import colorsys
 from lifxlan import LifxLAN
 #from lifxlan import BLUE, CYAN, GREEN, LifxLAN, ORANGE, PINK, PURPLE, RED, YELLOW
 
 
-def rgb_to_hsbk(rgb):
+white = (255, 255, 255)
+red = (255, 0, 0)
+orange = (255, 200, 0)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+cyan = (0, 255, 255)
+blue = (0, 0, 255)
+purple = (150, 0, 255)
+pink = (255, 0, 255)
+brightMax = 1
+bright = brightMax
+
+running = True
+
+
+#============================== Light settings ================================
+
+def rgb_to_hsbk(rgb, kelvin=3500):
     hsbk = colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2])
     hue = int(65535 * hsbk[0])
     sat = int(65535 * hsbk[1])
     bright = int(65535 * hsbk[2] / 255)
-    kelvin = 3500
     return [hue, sat , bright, kelvin]
 
 
-def notify(rgb_color, period, bright):
+def notify(rgb_color, kelvin, period, bright):
     # instantiate LifxLAN client, num_lights may be None (unknown).
     # In fact, you don't need to provide LifxLAN with the number of bulbs at all.
     # lifx = LifxLAN() works just as well. Knowing the number of bulbs in advance
     # simply makes initial bulb discovery faster.
 
-    color = rgb_to_hsbk(rgb_color)
+    color = rgb_to_hsbk(rgb_color, kelvin)
     color[2] = int(65535 * bright)
     rapid = True if period < 0.5 else False
-    print("bright: ", bright)
-    print("color: ", color)
+    #print("bright: ", bright)
+    #print("color: ", color)
 
     #print("Discovering lights...")
     lifx = LifxLAN(2)
     devices = lifx.get_lights()
     #print("\nFound {} light(s):\n".format(len(devices)))
 
-    print("setting light to: ", rgb_color)
+    #print("setting light to: ", rgb_color)
 
     # Set light color and brightness
     running = True
@@ -54,52 +73,51 @@ def notify(rgb_color, period, bright):
             pass
 
 
-def main():
-    red = (255, 0, 0)
-    orange = (255, 150, 0)
-    yellow = (255, 255, 0)
-    green = (0, 255, 0)
-    cyan = (0, 255, 255)
-    blue = (0, 0, 255)
-    purple = (150, 0, 255)
-    pink = (255, 0, 255)
-    brightMax = 0.5
-    bright = brightMax
+#============================== Light settings ================================
 
-    duration_secs = 0.5
+def main():
+    duration_secs = 1
     transition_time_ms = duration_secs * 1000
 
     #keyboard = Controller()
-    running = True
-    while running:
-        user_input = input("")
-        if user_input == 'r':
-            color = red
-        if user_input == 'o':
-            color = orange
-        if user_input == 'y':
-            color = yellow
-        if user_input == 'g':
-            color = green
-        if user_input == 'c':
-            color = cyan
-        if user_input == 'b':
-            color = blue
-        if user_input == 'p':
-            color = purple
-        if user_input == 'k':
-            color = pink
-        if user_input == 'q':
-            running = False
-            break
+    color = white
+    global running
 
-        notify(color, transition_time_ms, bright)
-        time.sleep(2*duration_secs)
+    while (running == True):
+        input = select.select([sys.stdin], [], [], 1)[0]
+        if input:
+            value = sys.stdin.readline().rstrip()
+
+            if (value == 'w'):
+                color = white
+            if (value == 'r'):
+                color = red
+            if (value == 'o'):
+                color = orange
+            if (value == 'y'):
+                color = yellow
+            if (value == 'g'):
+                color = green
+            if (value == 'c'):
+                color = cyan
+            if (value == 'b'):
+                color = blue
+            if (value == 'p'):
+                color = purple
+            if (value == 'k'):
+                color = pink
+            if (value == "q"):
+                running = False
+                break
+
+        global bright
+        kelvin = 3500
+        notify(color, kelvin, transition_time_ms, bright)
+        time.sleep(0.1)
         if bright == brightMax:
-            bright = 0
+            bright = 0.25
         else:
             bright = brightMax
-        print("bright:", bright)
 
 
 if __name__=="__main__":
--- weather.py
+++ weather.py
@@ -1,49 +1,98 @@
 # import modules
-#import python_weather
-import asyncio
-import os
+import requests, json
 import light_notify
+
 from lifxlan import LifxLAN
+from light_notify import white, red, orange, yellow, cyan, blue
+from bs4 import BeautifulSoup
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
 
-"""
-async def getweather():
-  # declare the client. format defaults to the metric system (celcius, km/h, etc.)
-  async with python_weather.Client(format=python_weather.IMPERIAL) as client:
 
-    # fetch a weather forecast from a city
-    weather = await client.get("New York")
+#The majority of the weather() function was found here:
+#https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
 
-    # returns the current day's forecast temperature (int)
-    print(weather.current.temperature)
+# Light settings
+DEF_kelvin = 3500
+DEF_period = 1
+DEF_bright = 0.75
+Max_bright = 0.75
 
-    # get the weather forecast for a few days
-    for forecast in weather.forecasts:
-      print(forecast.date, forecast.astronomy)
 
-      # hourly forecasts
-      for hourly in forecast.hourly:
-        print(f' --> {hourly!r}')
+def setfromrange(val, oldMin, oldMax, newMin, newMax):
+    val = math.min(val, oldMax)
+    val = math.max(val, oldMin)
+    tmp = (val - oldMin) / oldMax
+    result = int(newMin + tmp * (newMax - newMin))
+    result = math.min(result, newMax)
+    result = math.max(result, newMin)
+    return result
 
-if __name__ == "__main__":
-  # see https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
-  # for more details
-  if os.name == "nt":
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-  asyncio.run(getweather())
-"""
+def timeofday(time):
+    str_parts = time.split()
+    mil_time = str_parts[1].split(':')[0]
 
-def temperature(period, intensity):
-    light_notify.notify(RED, period, intensity)
+    if (time[-2:] == "PM"):
+        mil_time += 12
 
-def sun(period, intensitys):
-    light_notify.notify(YELLOW, period, intensity)
+    print("time:", mil_time)
 
-def rain(period, intensity):
-    light_notify.notify(BLUE, period, intensity)
+    kelvin = DEF_kelvin
+    period = DEF_period
+    bright = DEF_bright
+    light_notify.notify(red, kelvin, period, bright)
 
-def snow(period, intensity):
-    light_notify.notify(WHITE, period, intensity)
 
-main():
-    print("Hi, how are ya")
+def cloudcover(cover):
+    print("cover:", cover)
+    kelvin = DEF_kelvin
+    period = DEF_period
+    bright = DEF_bright
+    light_notify.notify(white, kelvin, period, bright)
+
+
+def temperature(temp):
+    print("temp:", temp)
+
+    kelvin = setfromrange(temp, 0, 100, 2500, 9000)
+    period = 2 - setfromrange(temp, 0, 100, 0, 1.75)
+    bright = setfromrange(temp, 0, 100, 0.5*Max_bright, Max_bright)
+    light_notify.notify(white, kelvin, period, bright)
+
+
+def weather(city, factor):
+    city = city.replace(" ", "+")
+    res = requests.get(
+        f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+    print("Searching...\n")
+    soup = BeautifulSoup(res.text, 'html.parser')
+    location = soup.select('#wob_loc')[0].getText().strip()
+    time = soup.select('#wob_dts')[0].getText().strip()
+    info = soup.select('#wob_dc')[0].getText().strip()
+    weather = soup.select('#wob_tm')[0].getText().strip()
+    print(location)
+    print(time)
+    print(info)
+    print(weather+"°C")
+
+    # Send result to control light
+    if (factor == "time" or factor == "t"):
+        timeofday(time)
+    if (factor == "cover" or factor == "c"):
+        cloudcover(info)
+    if (factor == "temp" or factor == "k"):
+        temperature(weather)
+
+
+def main():
+    city = input("Enter city name: ")
+    city = city+" weather"
+
+    factor = input("Enter factor (time, cover, temp): ")
+    weather(city, factor)
+
+
+if __name__=="__main__":
+    main()
